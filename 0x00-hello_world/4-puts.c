257b5ab849deb0ee56a52b12468761e9095b2efa
Lyydiah <udombosolydia@gmail.com>
2022-06-02 10:27:21 -0700

    Printed a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ *Main-Entry point
+ *Return:Always 0 (success)
+ */
 int main(void)
 {
-  puts ("Programming is like building a multilingual puzzle\n");
-  return (0);
+puts(stderr,"Programming is like building a multilingual puzzle\n");
+return (0);
 }
