0b30ec63698f2c60987f49f17aef1744a5cd51a2
Lyydiah <udombosolydia@gmail.com>
2022-06-09 14:30:27 -0700

    fixed

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,2 +1,25 @@
 void print_alphabet(void);
 void print_alphabet_x10(void);
+#include "main.h"
+
+
+
+/**
+
+ * main - check the code for Holberton School students.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+  
+{
+  
+  print_alphabet();
+  
+  return (0);
+  
+}
